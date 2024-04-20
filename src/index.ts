b8cc92f5c196e67d6ae14977e5c0170ef9fff709
Stephen Zhou <hi@hyoban.cc>
2024-04-20 21:34:56 +0800

    feat: support multi config, v3 only
    close #6

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ export async function activate(extContext: vscode.ExtensionContext) {
   const workspacePath =
     vscode.workspace.workspaceFolders?.[0]?.uri.fsPath ?? "";
   if (!workspacePath) return;
+
   const logger = vscode.window.createOutputChannel(
     "Tailwind CSS ClassName Highlight",
   );
@@ -20,45 +21,58 @@ export async function activate(extContext: vscode.ExtensionContext) {
   });
   extContext.subscriptions.push(logger, decorationType);
 
-  const tailwindcssPackageInfo = await getPackageInfo("tailwindcss", {
+  // handle multiple tailwind v3 config files
+  let tailwindV3ConfigPathList: string[] = fg
+    .globSync("./**/tailwind.config.{js,cjs,mjs,ts}", {
+      cwd: workspacePath,
+      ignore: ["**/node_modules/**"],
+    })
+    .map((p) => path.join(workspacePath, p));
+  let tailwindV3PackageEntryList: string[] = tailwindV3ConfigPathList.map((p) =>
+    resolveModule("tailwindcss", { paths: [p] }),
+  ) as string[];
+  if (tailwindV3PackageEntryList.length > 0) {
+    tailwindV3ConfigPathList = tailwindV3ConfigPathList.filter(
+      (_, index) => tailwindV3PackageEntryList[index],
+    );
+    tailwindV3PackageEntryList = tailwindV3PackageEntryList.filter(Boolean);
+  }
+
+  const workspaceTailwindPackageInfo = await getPackageInfo("tailwindcss", {
     paths: [workspacePath],
   });
-  if (!tailwindcssPackageInfo?.version || !tailwindcssPackageInfo?.rootPath) {
+  if (
+    (!workspaceTailwindPackageInfo?.version ||
+      !workspaceTailwindPackageInfo?.rootPath) &&
+    tailwindV3ConfigPathList.length === 0
+  ) {
     logger.appendLine("Tailwind CSS package not found");
     return;
   }
-  logger.appendLine(
-    `Detected Tailwind CSS version: ${tailwindcssPackageInfo.version}`,
-  );
 
-  const isV4 = tailwindcssPackageInfo.version.startsWith("4");
+  if (tailwindV3ConfigPathList.length > 1) {
+    logger.appendLine(
+      `Multiple Tailwind CSS config files found: ${tailwindV3ConfigPathList.map((p) => p).join(", ")}`,
+    );
+  } else {
+    logger.appendLine(
+      `Detected Tailwind CSS version: ${workspaceTailwindPackageInfo?.version}`,
+    );
+  }
 
-  const tailwindcssPackageEntry = resolveModule("tailwindcss", {
+  const isV4 = workspaceTailwindPackageInfo?.version?.startsWith("4") ?? false;
+  const globalTailwindPackageEntry = resolveModule("tailwindcss", {
     paths: [workspacePath],
-  });
-  if (!tailwindcssPackageEntry) {
+  })!;
+  if (isV4 && !globalTailwindPackageEntry) {
     logger.appendLine("Tailwind CSS package entry not found");
     return;
   }
 
-  let tailwindConfigPath = "";
+  const tailwindConfigPath: string[] = tailwindV3ConfigPathList;
   let cssFilePath = "";
 
-  if (!isV4) {
-    const configPath = fg
-      .globSync("./**/tailwind.config.{js,cjs,mjs,ts}", {
-        cwd: workspacePath,
-        ignore: ["**/node_modules/**"],
-      })
-      .map((p) => path.join(workspacePath, p))
-      .find((p) => fs.existsSync(p));
-    if (!configPath) {
-      logger.appendLine("Tailwind CSS config file not found");
-      return;
-    }
-    logger.appendLine(`Tailwind CSS config file found at ${configPath}`);
-    tailwindConfigPath = configPath;
-  } else {
+  if (isV4) {
     const configPath = fg
       .globSync("./**/*.css", {
         cwd: workspacePath,
@@ -84,58 +98,79 @@ export async function activate(extContext: vscode.ExtensionContext) {
     cssFilePath = configPath.at(0)!;
   }
 
-  const decoration = isV4
-    ? new DecorationV4(
-        workspacePath,
-        logger,
-        decorationType,
-        tailwindcssPackageEntry.replaceAll(".mjs", ".js"),
-        cssFilePath,
-      )
-    : new DecorationV3(
-        workspacePath,
-        logger,
-        decorationType,
-        path.resolve(tailwindcssPackageEntry, "../../"),
-        tailwindConfigPath,
+  const decorationList = isV4
+    ? [
+        new DecorationV4(
+          workspacePath,
+          logger,
+          decorationType,
+          globalTailwindPackageEntry.replaceAll(".mjs", ".js"),
+          cssFilePath,
+        ),
+      ]
+    : tailwindV3PackageEntryList.map(
+        (tailwindcssPackageEntry, index) =>
+          new DecorationV3(
+            workspacePath,
+            logger,
+            decorationType,
+            path.resolve(tailwindcssPackageEntry, "../../"),
+            tailwindConfigPath.at(index)!,
+          ),
       );
 
-  if (!decoration.checkContext()) return;
+  if (!decorationList.some((i) => i.checkContext())) return;
 
   const onReload = () => {
-    decoration.updateTailwindContext();
-    for (const element of vscode.window.visibleTextEditors)
-      decoration.decorate(element);
+    for (const i of decorationList) {
+      i.updateTailwindContext();
+    }
+    for (const element of vscode.window.visibleTextEditors) {
+      for (const i of decorationList) {
+        i.decorate(element);
+      }
+    }
   };
 
-  const fileWatcher = vscode.workspace.createFileSystemWatcher(
-    isV4 ? cssFilePath : tailwindConfigPath,
-  );
-  fileWatcher.onDidChange(onReload);
+  const fileWatcherList = [...tailwindConfigPath, cssFilePath].filter(Boolean);
+  for (const file of fileWatcherList) {
+    const fileWatcher = vscode.workspace.createFileSystemWatcher(file);
+    fileWatcher.onDidChange(onReload);
+    extContext.subscriptions.push(fileWatcher);
+  }
 
   extContext.subscriptions.push(
     vscode.commands.registerCommand(
       "tailwindcss-classname-highlight.reload",
       onReload,
     ),
-    fileWatcher,
   );
-
-  const decorate = decoration.decorate.bind(decoration);
 
   // on activation
   const openEditors = vscode.window.visibleTextEditors;
-  for (const element of openEditors) decorate(element);
+  for (const element of openEditors) {
+    for (const i of decorationList) {
+      i.decorate(element);
+    }
+  }
 
   // on editor change
   extContext.subscriptions.push(
-    vscode.window.onDidChangeActiveTextEditor(decorate),
+    vscode.window.onDidChangeActiveTextEditor((editor) => {
+      if (!editor) return;
+      for (const i of decorationList) {
+        i.decorate(editor);
+      }
+    }),
     vscode.workspace.onDidChangeTextDocument((event) => {
       if (event.document.languageId === "Log") return;
       const openEditor = vscode.window.visibleTextEditors.find(
         (editor) => editor.document.uri === event.document.uri,
       );
-      decorate(openEditor);
+      if (!openEditor) return;
+      for (const i of decorationList) {
+        i.decorate(openEditor);
+      }
     }),
   );
 }
