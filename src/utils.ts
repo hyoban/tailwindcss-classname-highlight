3a584fef6ca304f3382d4a7014c7c834e49eb81d
Stephen Zhou <hi@hyoban.cc>
2024-06-21 22:13:09 +0800

    chore: fix typecheck

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 export const defaultIdeMatchInclude = [
   // String literals
-  // eslint-disable-next-line regexp/strict
-  /(["'`])[^\1]*?\1/g,
+  // eslint-disable-next-line no-control-regex
+  /(["'`])[^\u0001]*?\1/g,
   // CSS directives
   /(@apply)[^;]*;/g,
 ]
