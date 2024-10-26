89b92b4feb378eeac3b8a07e199493c9527a66af
Stephen Zhou <38493346+hyoban@users.noreply.github.com>
2024-10-26 21:11:30 +0800

    fix: update match include regex

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,6 @@
 export const defaultIdeMatchInclude = [
   // String literals
-  // eslint-disable-next-line no-control-regex
-  /(["'`])[^\u0001]*?\1/g,
+  /(["'`])(.*?)\1/g,
   // CSS directives
   /(@apply)[^;]*;/g,
 ]
